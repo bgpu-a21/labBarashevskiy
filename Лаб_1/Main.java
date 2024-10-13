c0fd731aa4b4d1ee503f63548bd0ccd41a4981ae
Digering <63855521+Digering@users.noreply.github.com>
2024-10-13 13:18:11 +0300

    Update Main.java

--- Лаб_1/Main.java
+++ Лаб_1/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
 
 public class Main {
@@ -8,6 +11,9 @@ public class Main {
 
         // Определяем функцию для интегрирования (например, синус)
         Function<Double, Double> function = Math::sin; // Измените функцию при необходимости
+        
+                // Список для хранения результатов и времени выполнения
+        List<Integral> results = new ArrayList<>();
 
         // Цикл по количеству потоков от 1 до 20
         for (int n = 1; n <= 20; n++) {
@@ -17,12 +23,20 @@ public class Main {
                 double result = integral.calculate(n); // Вычисляем интеграл
                 long endTime = System.currentTimeMillis(); // Конец измерения времени
                 double elapsedTime = endTime - startTime; // Вычисляем затраченное время
+                
+                results.add(new Integral(n, elapsedTime, result));
 
                 // Выводим статистику
-                System.out.printf("%d - %.1f мс. (Результат: %.6f)%n", n, elapsedTime, result);
+                // System.out.printf("%d - %.1f мс. (Результат: %.6f)%n", n, elapsedTime, result);
             } catch (Exception e) {
                 e.printStackTrace(); // Обрабатываем возможные исключения
             }
+        }
+        results.sort(Comparator.comparingDouble(Integral::getElapsedTime));
+
+        // Выводим отсортированные данные
+        for (Integral result : results) {
+            System.out.printf("%d поток(ов) - %.1f мс. (Результат: %.6f)%n", result.getThreads(), result.getElapsedTime(), result.getResult());
         }
     }
 }
